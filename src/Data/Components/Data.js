f32f875e05409fd96b36a3b75889cd15751bb68f
3lv27 <elvin.gm27@gmail.com>
2018-02-17 16:57:42 +0100

    basic structure

---
+++ src/Data/Components/Data.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Data = props => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+Data.propTypes = {
+  
+};
+
+export default Data;
---
+++ src/Data/Container/DataContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Data from '../Components/Data'
+
+class DataContainer extends Component {
+  render() {
+    return (
+      <div>
+        <Data />
+      </div>
+    );
+  }
+}
+
+export default DataContainer;
