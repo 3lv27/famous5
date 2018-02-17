7fce0f5bee8977afcb29120ac84ff47f715ca009
Clara Ameller <claraameller7@gmail.com>
2018-02-17 17:51:00 +0100

    Start/stop interval WIP

--- src/Home/Container/HomeContainer.js
+++ src/Home/Container/HomeContainer.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 import Home from '../Components/Home'
 
-class DataContainer extends Component {
+class HomeContainer extends Component {
+  state = {
+    money: 0, // cents
+    running: false,
+    button_text: 'Start',
+    time: 0
+  }
+
+  startStopCick() {
+    if (!this.state.running) {
+      this.setState({
+        button_text: 'Stop',
+        time: setInterval(() => {
+          this.saveMoney()
+        }, 1000) // 1 sec
+      })
+      // this.state.running = !this.state.running
+    }
+  }
+
+  saveMoney() {
+    this.setState({
+      money: this.state.money += 1 // 1 cent
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.time);
+  }
+
   render() {
     return (
       <div>
         <Home />
+        <button onClick={this.startStopCick()}>
+          {this.state.button_text}
+        </button>
+        <div>Time: {this.time}</div>
       </div>
     );
   }
 }
 
-export default DataContainer;
+export default HomeContainer;
